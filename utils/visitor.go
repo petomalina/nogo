9a104fdc9c51472f917ac54028383a758407e593
Peter Malina <peter.malina@flowup.cz>
2018-03-03 16:43:23 +0100

    utils/visitor: removed unnecessary visitor types

--- utils/visitor.go
+++ utils/visitor.go
@@ -5,7 +5,3 @@ import "go/ast"
 type Visitor interface {
 	Run(ast.Node) []error
 }
-
-type VisitorFunc func(n ast.Node) ast.Visitor
-
-func (f VisitorFunc) Visit(n ast.Node) ast.Visitor { return f(n) }
